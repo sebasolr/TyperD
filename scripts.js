fcbf52fa8f49164a3ec31cb900bd1a989287fc35
sebasolr <seba.olr@gmail.com>
2022-06-09 12:34:58 -0400

    avance

--- scripts.js
+++ scripts.js
@@ -22,17 +22,49 @@ const words = ['californication','plataforma5','black','summer','flea','aeroplan
 function init(){
     // load word from array
     showWord(words);
+    // comenzar con la palabra input
+    wordInput.addEventListener('input',starMatch)
     //cuenta regresiva 10 segundo
     setInterval(countdown,1000);
     //check juego
     setInterval(checkStatus,50);
 }
+
+// comenzar juego.
+function starMatch(){
+    if(matchWords()){ //si la palabra es correcta el jugador obtedra puntos,si falla fin del juego
+        playing= true;
+        time= 11;
+        showWord(words);
+        wordInput.value='';
+        score++;
+    }
+
+    if(score === -1){
+        ScoreDisplay.innerHTML= 0;
+    }else{
+        ScoreDisplay.innerHTML=score;
+
+    }
+}
+function matchWords(){
+    if(wordInput.value ===currentWord.innerHTML){
+        message.innerHTML = 'Eri seco!!';
+        return true
+    }else{
+        message.innerHTML='';
+        return false
+    }
+}
+
 // generar palabra random
 function showWord(words){
     const randIndex = Math.floor(Math.random()*words.length);
     // salida de la palabra random
     currentWord.innerHTML=words[randIndex];
 }
+// comparar palabra random con palabra input
+
 // countdown timer
 function countdown(){
 // hacer que el tiempo corra
@@ -48,10 +80,8 @@ seconds.innerHTML=time;
 function checkStatus(){
     if(playing=== false && time === 0){
         message.innerHTML='Perdiste';
+        score=-1;
     }
 }
 
-// funciones
 
-// let word = words[Math.floor(Math.random()*words.length)];
-// wordInput.innerHTML= 'word';
